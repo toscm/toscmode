b6ac557c0e7db7ef011eea313b5ca14dc7adb1e3
Tobias Schmidt <tobias2.schmidt@ukr.de>
2024-10-13 13:26:17 +0200

    Make widget persistent across sessions

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,99 @@
 import {
+    ILayoutRestorer,
     JupyterFrontEnd,
-    JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+    JupyterFrontEndPlugin,
+} from '@jupyterlab/application'
 
-import { ISettingRegistry } from '@jupyterlab/settingregistry';
-import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
-import { Widget } from '@lumino/widgets';
+import {
+    ICommandPalette,
+    MainAreaWidget,
+    WidgetTracker,
+} from '@jupyterlab/apputils'
 
-/**
-* Initialization data for the jupyterlab_toscmode extension.
-*/
-const plugin: JupyterFrontEndPlugin<void> = {
-    id: 'jupyterlab_toscmode:plugin',
-    description: 'Adds a Showcase Mode to Jupyter Lab',
-    autoStart: true,
-    optional: [ISettingRegistry],
-    requires: [ICommandPalette],
-    activate: (
-        app: JupyterFrontEnd,
-        palette: ICommandPalette,
-        settingRegistry: ISettingRegistry | null
-    ) => {
-        console.log('JupyterLab extension jupyterlab_toscmode is activated!');
-        console.log('ICommandPalette:', palette);
+import { Widget } from '@lumino/widgets'
 
-        if (settingRegistry) {
-            settingRegistry
-            .load(plugin.id)
-            .then(settings => {
-                console.log(
-                    'jupyterlab_toscmode settings loaded:',
-                    settings.composite
-                );
-            })
-            .catch(reason => {
-                console.error(
-                    'Failed to load settings for jupyterlab_toscmode.',
-                    reason
-                );
-            });
-        }
+interface APODResponse {
+    copyright: string
+    date: string
+    explanation: string
+    media_type: 'video' | 'image'
+    title: string
+    url: string
+}
 
-        // Define a widget creator function,
-        // then call it to make a new widget
-        const newWidget = () => {
-            // Create a blank content widget inside of a MainAreaWidget
-            const content = new Widget();
-            const widget = new MainAreaWidget({ content });
-            widget.id = 'apod-jupyterlab';
-            widget.title.label = 'Astronomy Picture';
-            widget.title.closable = true;
-            return widget;
-        };
-        let widget = newWidget();
-
-        // Add an application command
-        const command: string = 'apod:open';
-        app.commands.addCommand(command, {
-            label: 'Random Astronomy Picture',
-            execute: () => {
-                // Regenerate the widget if disposed
-                if (widget.isDisposed) {
-                    widget = newWidget();
-                }
-                if (!widget.isAttached) {
-                    // Attach the widget to the main work area if it's not there
-                    app.shell.add(widget, 'main');
-                }
-                // Activate the widget
-                app.shell.activateById(widget.id);
-            }
-        });
-
-        // Add the command to the palette.
-        palette.addItem({ command, category: 'Tutorial' });
+class APODWidget extends Widget {
+    constructor() {
+        super()
+        this.addClass('my-apodWidget')
+        this.img = document.createElement('img')
+        this.node.appendChild(this.img)
+        this.summary = document.createElement('p')
+        this.node.appendChild(this.summary)
     }
-};
+    readonly img: HTMLImageElement
+    readonly summary: HTMLParagraphElement
+    async updateAPODImage(): Promise<void> {
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${this.randomDate()}`)
+        if (!response.ok) {
+            const data = await response.json()
+            this.summary.innerText = data.error ? data.error.message : response.statusText;
+            return
+        }
+        const data = (await response.json()) as APODResponse
+        if (data.media_type === 'image') {
+            this.img.src = data.url
+            this.img.title = data.title
+            this.summary.innerText = data.title
+            if (data.copyright) { this.summary.innerText += ` (Copyright ${data.copyright})` }
+        } else {
+            this.summary.innerText = 'Random APOD fetched was not an image.'
+        }
+    }
 
-export default plugin;
+    randomDate(): string {
+        const start = new Date(2010, 1, 1)
+        const end = new Date()
+        const randomDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+        return randomDate.toISOString().slice(0, 10)
+    }
+}
+
+function activate(
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    restorer: ILayoutRestorer | null
+) {
+    console.log('JupyterLab extension toscmode is activated!')
+    let widget: MainAreaWidget<APODWidget>
+    const command: string = 'apod:open'
+    app.commands.addCommand(command, {
+        label: 'Random Astronomy Picture',
+        execute: () => {
+            if (!widget || widget.isDisposed) {
+                const content = new APODWidget()
+                widget = new MainAreaWidget({ content })
+                widget.id = 'apod-jupyterlab'
+                widget.title.label = 'Astronomy Picture'
+                widget.title.closable = true
+            }
+            if (!tracker.has(widget)) { tracker.add(widget) } // Track the state of the widget for later restoration
+            if (!widget.isAttached) { app.shell.add(widget, 'main') } // Attach the widget to the main work area if it's not there
+            widget.content.updateAPODImage() // Activate the widget
+            app.shell.activateById(widget.id)
+        }
+    })
+    palette.addItem({ command, category: 'Tutorial' }) // Add the command to the palette.
+    let tracker = new WidgetTracker<MainAreaWidget<APODWidget>>({ namespace: 'apod' }) // Track and restore the widget state
+    if (restorer) { restorer.restore(tracker, { command, name: () => 'apod'}) }
+}
+
+const plugin: JupyterFrontEndPlugin<void> = {
+    id: 'jupyterlab-apod',
+    description: 'Show a random NASA Astronomy Picture of the Day in a JupyterLab panel.',
+    autoStart: true,
+    requires: [ICommandPalette],
+    optional: [ILayoutRestorer],
+    activate: activate,
+}
+
+export default plugin
